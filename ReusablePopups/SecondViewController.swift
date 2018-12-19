9aedf578354b03d766d916a54b0ac6895be93f96
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-12-19 16:04:54 +0530

    Initial Commit

---
+++ ReusablePopups/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ReusablePopups
+//
+//  Created by Prashant G on 12/19/18.
+//  Copyright © 2018 Prashant G. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
